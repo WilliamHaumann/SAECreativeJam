c047fcb121feecb30703239fdb76287ee0407d86
William <William.haumann@gmail.com>
2020-05-29 14:00:15 +1000

    Name changes and making a menu

--- Plane Noir/Assets/Scripts/InventoryHandler.cs
+++ Plane Noir/Assets/Scripts/InventoryHandler.cs
@@ -8,17 +8,7 @@ public class InventoryHandler : MonoBehaviour
 
     public List<Image> inventoryImage = new List<Image>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+ 
     public void activateImage(int arrayId)
     {
         inventoryImage[arrayId].gameObject.SetActive(true);
